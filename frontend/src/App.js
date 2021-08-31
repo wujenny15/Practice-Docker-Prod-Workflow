4af59d26d9913841ef5502660b6f4a0b82bb9da0
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2021-08-31 15:41:23 +1000

    Add Dockerfile.dev file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi There! jenny
         </a>
       </header>
     </div>
